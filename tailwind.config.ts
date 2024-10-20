f622173b7ce65df72681a304bf8df8eb76e76129
Kei <yoshikoshikei95@gmail.com>
2024-10-19 22:39:07 -0700

    feat: add Alchemy account kit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,27 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+import { createColorSet, withAccountKitUi } from "@account-kit/react/tailwind";
+ 
+export default withAccountKitUi(
+  {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          background: "var(--background)",
+          foreground: "var(--foreground)",
+        },
       },
     },
+    plugins: [],
   },
-  plugins: [],
-};
-export default config;
+  {
+    // override account kit themes
+    colors: {
+      "btn-primary": createColorSet("#E82594", "#FF66CC"),
+      "fg-accent-brand": createColorSet("#E82594", "#FF66CC"),
+    },
+  },
+);
